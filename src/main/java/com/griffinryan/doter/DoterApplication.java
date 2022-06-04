d376d73deac3a3ac571f8e919ccf3433a1835a51
griffinryan <glryan@uw.edu>
2022-06-04 15:01:54 -0700

    Deployed some changes to main!

--- src/main/java/com/griffinryan/doter/DoterApplication.java
+++ src/main/java/com/griffinryan/doter/DoterApplication.java
@@ -1,15 +1,20 @@
 package com.griffinryan.doter;
 
-import com.griffinryan.doter.editor.CodeEditor;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import com.griffinryan.doter.editor.CodeEditor;
+
+import java.io.IOException;
 
 /**
  * An example application which demonstrates use of a
@@ -17,69 +22,16 @@ import javafx.stage.Stage;
  * a JavaFX WebView.
  */
 public class DoterApplication extends Application {
-	// some sample code to be edited.
-	static final private String editingCode =
-			"import javafx.application.Application;\n" +
-					"import javafx.scene.Scene;\n" +
-					"import javafx.scene.web.WebView;\n" +
-					"import javafx.stage.Stage;\n" +
-					"\n" +
-					"/** Sample code editing application wrapping an editor in a WebView. */\n" +
-					"public class CodeEditorExample extends Application {\n" +
-					"  public static void main(String[] args) { launch(args); }\n" +
-					"  @Override public void start(Stage stage) throws Exception {\n" +
-					"    WebView webView = new WebView();\n" +
-					"    webView.getEngine().load(\"http://codemirror.net/mode/groovy/index.html\");\n" +
-					"    final Scene scene = new Scene(webView);\n" +
-					"    webView.prefWidthProperty().bind(scene.widthProperty());\n" +
-					"    webView.prefHeightProperty().bind(scene.heightProperty());\n" +
-					"    stage.setScene(scene);\n" +
-					"    stage.show();\n" +
-					"  }\n" +
-					"}";
-
-	public static void main(String[] args) { launch(args); }
-	@Override public void start(Stage stage) throws Exception {
-		// create the editing controls.
-		Label title = new Label("Editing: CodeEditor.java");
-		title.setStyle("-fx-font-size: 20;");
-		final Label labeledCode = new Label(editingCode);
-		final CodeEditor editor = new CodeEditor(editingCode);
-		final Button revertEdits = new Button("Revert edits");
-		revertEdits.setOnAction(new EventHandler<ActionEvent>() {
-			@Override public void handle(ActionEvent actionEvent) {
-				editor.revertEdits();
-			}
-		});
-		final Button copyCode = new Button(
-				"Take a snapshot from the editor and set a revert point"
-		);
-		copyCode.setOnAction(new EventHandler<ActionEvent>() {
-			@Override public void handle(ActionEvent actionEvent) {
-				labeledCode.setText(editor.getCodeAndSnapshot());
-				System.out.println(editor.getCodeAndSnapshot());
-			}
-		});
-
-		// layout the scene.
-		final HBox hlayout = new HBox(10);
-		hlayout.getChildren().addAll(copyCode, revertEdits);
-
-		final VBox layout = new VBox(10);
-		layout.getChildren().addAll(title, editor, hlayout);
-
-		/*
-		final VBox layout = VBox.create().spacing(10).children(
-				title,
-				editor,
-				HBoxBuilder.create().spacing(10).children(copyCode, revertEdits).build(),
-				labeledCode
-		).build();	*/
-		layout.setStyle("-fx-background-color: cornsilk; -fx-padding: 10;");
-
-		// display the scene.
-		final Scene scene = new Scene(layout);
+	@Override
+	public void start(Stage stage) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(DoterApplication.class.getResource("hello-view.fxml"));
+		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+		stage.setTitle("Hello!");
 		stage.setScene(scene);
 		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch();
 	}
 }
