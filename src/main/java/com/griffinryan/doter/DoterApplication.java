8f95e7111231d53f84300b015ec94e6b26fc8423
griffinryan <glryan@uw.edu>
2022-06-04 17:24:36 -0700

    Deployed some changes to main!

--- src/main/java/com/griffinryan/doter/DoterApplication.java
+++ src/main/java/com/griffinryan/doter/DoterApplication.java
@@ -33,20 +33,11 @@ public class DoterApplication extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		/*
-		FXMLLoader fxmlLoader = new FXMLLoader(DoterApplication.class.getResource("doter-view.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 900, 600);
-		stage.setTitle("Hello!");
-		stage.setScene(scene);
-		stage.show();
-		*/
-
 		FXMLLoader fxmlLoader = new FXMLLoader(DoterApplication.class.getResource("doter-view.fxml"));
 
-		// Create a new MonacoFX Editor node.
+		/* Create a new MonacoFX editor node and SplitPane to use. */
 		MonacoFX monaco = new MonacoFX();
 		SplitPane pane = new SplitPane(monaco);
-		configMonacoNode(monaco, pane);
 
 		// set initial text.
 		monaco.getEditor().getDocument().setText(
@@ -67,45 +58,6 @@ public class DoterApplication extends Application {
 		primaryStage.setTitle("MonacoFX Demo (running on JDK " + System.getProperty("java.version") + ")");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-	}
-
-	public static void configMonacoNode(MonacoFX monaco, SplitPane pane){
-		// custom language based on
-		// https://microsoft.github.io/monaco-editor/playground.html#extending-language-services-custom-languages
-		LanguageSupport language = new LanguageSupport() {
-			@Override
-			public String getName() {
-				return "doterjava";
-			}
-			@Override
-			public FoldingProvider getFoldingProvider() {
-				// register custom code folds:
-				return editor -> new Folding[]{new Folding(1,5),new Folding(7,10)};
-			}
-			@Override
-			public MonarchSyntaxHighlighter getMonarchSyntaxHighlighter() {
-				// custom syntax highlighter: currently, only monarch is supported.
-				return () -> """
-						tokenizer: {
-						    root: [
-						      [/\\[error.*/, "custom-error"],
-						      [/\\[notice.*/, "custom-notice"],
-						      [/\\[info.*/, "custom-info"],
-						      [/\\[[a-zA-Z 0-9:]+\\]/, "custom-date"],
-						    ]
-						}""";
-			}
-		};
-
-		monaco.getEditor().registerLanguage(language);
-
-		// register custom theme for the custom language
-		monaco.getEditor().registerTheme(new EditorTheme("doterjava-theme", "vs-dark", true,
-				new Rule("custom-info", "808080"),
-				new Rule("custom-error", "ff0000", null, null,null, "bold"),
-				new Rule("custom-notice", "ffa500"),
-				new Rule("custom-date", "008800")
-		));
 	}
 
 	// Color paint = new Color(0.0, 0.3207, 0.52, 0.7572);
