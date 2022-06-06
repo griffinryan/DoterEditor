a46b0501a292bf40e86b9a692100749e05371db5
griffinryan <glryan@uw.edu>
2022-06-06 12:30:08 -0700

    Added JSON workspace configuration feature

--- src/main/java/com/griffinryan/doter/editor/Workspace.java
+++ src/main/java/com/griffinryan/doter/editor/Workspace.java
@@ -1,6 +1,11 @@
 package com.griffinryan.doter.editor;
 
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Workspace {
 
@@ -9,9 +14,10 @@ public class Workspace {
 	private String fileLocation;
 	private String directoryName;
 	private String directoryLocation;
-	private String savedDocument;
-
 	private String fileExtension;
+	private JSONObject jsonFile;
+	private String savedDocument;
+	private boolean hasRecent;
 
 	public Workspace(String ext){
 		this.fileExtension = ext;
@@ -24,6 +30,44 @@ public class Workspace {
 		this.directoryName = directory;
 		this.directoryLocation = path;
 		this.fileExtension = ext;
+		this.hasRecent = checkHasRecent();
+		jsonFile = new JSONObject();
+
+		if(!hasRecent){
+			createConfig();
+		} else{
+			this.fileName = jsonFile.getString("fileName");
+			this.fileLocation = jsonFile.getString("fileLocation");
+			this.directoryName = jsonFile.getString("directoryName");
+			this.directoryLocation = jsonFile.getString("directoryLocation");
+			this.fileExtension = jsonFile.getString("fileExtension");
+		}
+	}
+
+	private void createConfig(){
+		String home = System.getProperty("user.home");
+		File configDir = new File(home + "/.config/doter");
+		File configFile = new File(configDir, "doter.json");
+
+		jsonFile.put("fileName", "void");
+		jsonFile.put("directoryName", "void");
+		jsonFile.put("fileLocation", "void");
+		jsonFile.put("directoryLocation", "void");
+		jsonFile.put("fileExtension", "void");
+	}
+
+	private boolean checkHasRecent(){
+		String home = System.getProperty("user.home");
+		Path tempDirectory = null;
+
+		try {
+			tempDirectory = Files.createTempDirectory(home + "/.config/doter");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		assert tempDirectory != null;
+		return !Files.notExists(tempDirectory);
 	}
 
 	public void instCurrentFile(String s){
@@ -87,4 +131,11 @@ public class Workspace {
 		this.fileExtension = fileExtension;
 	}
 
+	public JSONObject getJsonFile() {
+		return jsonFile;
+	}
+
+	public void setJsonFile(JSONObject jsonFile) {
+		this.jsonFile = jsonFile;
+	}
 }
