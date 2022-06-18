cec7a6505cb78ebf9becb68aaee3086891914bc7
griffinryan <glryan@uw.edu>
2022-06-18 11:08:15 -0700

    Deployed some changes to main!

--- src/main/java/com/griffinryan/doter/editor/Explorer.java
+++ src/main/java/com/griffinryan/doter/editor/Explorer.java
@@ -17,22 +17,18 @@ public class Explorer extends EditorTool {
 			new Image(Objects.requireNonNull(getClass().getResourceAsStream("folder.png")));
 	/*TreeItem<String> rootNode =
 			new TreeItem<String>("MyCompany Human Resources", rootIcon);*/
+
 	private TreeView<String> finder;
 	private List<Item> items;
 	private TreeItem<String> rootNote;
-
 
 	public Explorer(Workspace theWorkspace){
 
 		if(theWorkspace.isHasRecent() && theWorkspace.getCurrentDirectory().exists()){
 			// it has a directory to make an explorer of.
-
-
 		} else {
 			// it does not have a directory. maybe show a button to open one.
-			this.finder = new TreeView<>();
 		}
-
 	}
 
 	public TreeView<String> getFinder() {
