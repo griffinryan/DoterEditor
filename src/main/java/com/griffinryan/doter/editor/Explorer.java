14a38c5e057bafdfb54be96f5a0bbe4c487348f3
griffinryan <glryan@uw.edu>
2022-06-18 11:14:39 -0700

    Deployed some changes to main!

--- src/main/java/com/griffinryan/doter/editor/Explorer.java
+++ src/main/java/com/griffinryan/doter/editor/Explorer.java
@@ -11,21 +11,22 @@ import java.util.Objects;
 
 public class Explorer extends EditorTool {
 
+	private TreeView<String> finder;
+	private List<Item> items;
+	private TreeItem<String> rootNode;
+
 	private final Node rootIcon =
 			new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("small.png"))));
 	private final Image folderIcon =
 			new Image(Objects.requireNonNull(getClass().getResourceAsStream("folder.png")));
-	/*TreeItem<String> rootNode =
-			new TreeItem<String>("MyCompany Human Resources", rootIcon);*/
-
-	private TreeView<String> finder;
-	private List<Item> items;
-	private TreeItem<String> rootNote;
 
 	public Explorer(Workspace theWorkspace){
 
 		if(theWorkspace.isHasRecent() && theWorkspace.getCurrentDirectory().exists()){
 			// it has a directory to make an explorer of.
+			this.rootNode = new TreeItem<>("MyCompany Human Resources", rootIcon);
+
+
 		} else {
 			// it does not have a directory. maybe show a button to open one.
 		}
