998b8bcbd285e496bcb5b51bc6ea37e3b237563e
griffinryan <glryan@uw.edu>
2022-06-27 12:34:33 -0700

    fixed folder icon in Explorer View

--- src/main/java/com/griffinryan/doter/editor/Explorer.java
+++ src/main/java/com/griffinryan/doter/editor/Explorer.java
@@ -24,7 +24,7 @@ public class Explorer extends EditorTool {
 	public Explorer(Workspace workspace){
 		this.fileView = new TreeView<>(new SimpleFileTreeItem(workspace.getCurrentDirectory()));
 
-		fileView.setCellFactory(new Callback<TreeView<File>, TreeCell<File>>() {
+		fileView.setCellFactory(new Callback<>() {
 
 			public TreeCell<File> call(TreeView<File> tv) {
 				return new TreeCell<>() {
@@ -37,15 +37,16 @@ public class Explorer extends EditorTool {
 						Font f = new Font("SourceSansPro-Regular.ttf", 14);
 						setFont(f);
 
-						for(int i = 0; i < this.getChildren().size(); i++){
+						/*
+						for (int i = 0; i < this.getChildren().size(); i++) {
 							File temp = this.getTreeView().getTreeItem(i).getValue();
-							if(temp.isDirectory()){
+							if (temp.isDirectory()) {
 								setGraphic(folderIcon);
-							} else if (temp.isFile()){
+							} else if (temp.isFile()) {
 								setGraphic(rootIcon);
 							}
 						}
-						// Set icons with for-loop above.
+						// Set icons with for-loop above.	*/
 					}
 
 				};
@@ -56,49 +57,4 @@ public class Explorer extends EditorTool {
 	public TreeView<File> getFileView() {
 		return fileView;
 	}
-
-	public void setFileView(TreeView<File> fileView) {
-		this.fileView = fileView;
-	}
-
-	/*
-	public Explorer(Workspace theWorkspace){
-		this.items = new ArrayList<>(theWorkspace.getFileGroup().length);
-
-		for(int i = 0; i < theWorkspace.getFileGroup().length; i++){
-			this.items.add(new Item(theWorkspace.getFileGroup()[i], theWorkspace.getCurrentDirectory()));
-		}
-
-		this.rootNode = new TreeItem<>("Doter Project", rootIcon);
-
-		for(Item item : items){
-			TreeItem<String> itemLeaf = new TreeItem<>(item.getCurrentFile().getName());
-			boolean found = false;
-			for (TreeItem<String> depNode : rootNode.getChildren()) {
-				if (depNode.getValue().contentEquals(item.getFolder())){
-					depNode.getChildren().add(itemLeaf);
-					found = true;
-					break;
-				}
-			}
-			if (!found) {
-				TreeItem<String> depNode = new TreeItem<String>(
-						item.getFolder(),
-						new ImageView(folderIcon)
-				);
-				rootNode.getChildren().add(depNode);
-				depNode.getChildren().add(itemLeaf);
-			}
-		}
-
-		this.treeView = new TreeView<>(rootNode);
-		treeView.setBackground(bg);
-
-		if(theWorkspace.isHasRecent() && theWorkspace.getCurrentDirectory().exists()){
-			// it has a directory to make an explorer of.
-		} else {
-			// it does not have a directory. maybe show a button to open one.
-		}
-	}*/
-
 }
